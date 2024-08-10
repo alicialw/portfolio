52cc4892ccea1371093903ea2002934b3afb08e3
alicialw <alicia.ngxn@gmail.com>
2024-08-11 03:44:21 +0700

    Transitions

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".load1 .load2");
+    elements.forEach(element => {
+        element.classList.add("load");
+    });
+
+});
+
+window.addEventListener("loading", function() {
+    const elements = document.querySelectorAll("*");
+
+    elements.forEach(element => {
+        element.style.opacity = 1;
+    });
+
+    document.body.style.display = "block"; // Display the body after all elements are loaded
+});
