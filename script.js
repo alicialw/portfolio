9d458c86b0cc9050fb9018354ebd6e1c67ae331c
alicialw <alicia.ngxn@gmail.com>
2024-08-14 23:26:01 +0700

    Focus on CDs

--- script.js
+++ script.js
@@ -36,6 +36,74 @@ window.addEventListener("loading", function() {
     document.body.style.display = "flex"; 
 });
 
+
+
+
+const worksList = document.querySelector('.works_list');
+const worksListItems = document.querySelectorAll('.works_list li');
+
+const worksListMiddle = worksList.offsetTop + (worksList.offsetHeight / 2);
+let topItemIndex = 0;
+
+const getItemTop = (item) => {
+    const marginTop = parseInt(window.getComputedStyle(item).marginTop);
+    return item.offsetTop - worksList.scrollTop - marginTop;
+};
+
+const getItemHeight = (item) => {
+    const marginTop = parseInt(window.getComputedStyle(item).marginTop);
+    const marginBottom = parseInt(window.getComputedStyle(item).marginBottom);
+    return item.offsetHeight + marginTop + marginBottom;
+};
+
+worksList.addEventListener('scroll', function() {
+    worksListItems.forEach((item, index) => {
+        const itemTop = getItemTop(item);
+        const itemHeight = getItemHeight(item);
+
+        if (window.matchMedia('(min-aspect-ratio: 4/5)').matches) {
+            if (itemTop < worksListMiddle && itemTop + itemHeight > worksListMiddle) {
+                if (index !== topItemIndex) {
+                    worksListItems[topItemIndex].style.transform = 'rotateX(-60deg) rotateY(0deg) rotateZ(45deg)';
+                    topItemIndex = index;
+                    item.style.transform = 'translateX(-50%) translateY(0) rotateX(-60deg) rotateY(0deg) rotateZ(0deg)';
+                    item.classList.add('focus');
+                    wvideos.forEach((video, i) => {
+                        if (i !== index) {
+                            video.style.filter = defaultFilters[i];
+                        } else {
+                            video.style.filter = 'none';
+                        }
+                    });
+                    wlinks.forEach((link, i) => {
+                        if (i === index) {
+                            link.style.opacity = 1;
+                        } else {
+                            link.style.opacity = 0;
+                        }
+                    });
+                }
+            }
+        } else if (window.matchMedia('(max-aspect-ratio: 4/5)').matches) {
+            if (itemTop < worksListMiddle && itemTop + itemHeight > worksListMiddle) {
+                if (index !== topItemIndex) {
+                    worksListItems[topItemIndex].style.transform = 'rotateX(-60deg) rotateY(0deg) rotateZ(45deg)';
+                    topItemIndex = index;
+                    item.style.transform = 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
+                    item.style.margin = 'var(--rmg) 0';
+                    wvideos.forEach((video, i) => {
+                        if (i !== index) {
+                            video.style.filter = defaultFilters[i];
+                        } else {
+                            video.style.filter = 'none';
+                        }
+                    });
+                }
+            }
+        }
+    });
+});
+
 const wlinks = document.querySelectorAll('.works_list a');
 const wvideos = document.querySelectorAll('.works_list video, .works_list img');
 const defaultFilters = [];
@@ -45,15 +113,7 @@ wvideos.forEach(wvideo => {
 });
 
 
-wlinks.forEach((wlinks, index) => {
-    wlinks.addEventListener('mouseover', () => {
-        wvideos[index].style.filter = 'none';
-    });
 
-    wlinks.addEventListener('mouseout', () => {
-        wvideos[index].style.filter = defaultFilters[index];
-    });
-});
 
 const toggleButton = document.getElementById('toggleAbout');
 const aboutLyrics = document.querySelector('.about_lyrics');
@@ -97,3 +157,5 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
