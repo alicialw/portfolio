57fe3d3f9ec3463d72acdbfc3bb92bc962e40287
alicialw <alicia.ngxn@gmail.com>
2024-08-16 21:26:33 +0700

    Transition

--- script.js
+++ script.js
@@ -9,34 +9,24 @@ function showPage() {
     var main = document.getElementById("main");
 
     loader.style.opacity = "0";
-    main.style.display = "flex";
 
     setTimeout(() => {
         loader.style.display = "none";
-    }, 500); 
+        main.style.display = "flex";
+
+        setTimeout(() => {
+            const elements = document.querySelectorAll(".load1, .load2, load3, .load4");
+            elements.forEach(element => {
+                setTimeout(() => {
+                    element.classList.add("load");
+                }, 0);
+            });
+        }, 500);
+    }, 500);
 }
 
-window.onload = function() {
-    clearTimeout(loaderVar);
-    showPage();
-};
-
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll(".load1 .load2");
-    elements.forEach(element => {
-        element.classList.add("load");
-    });
-
-});
-
-window.addEventListener("loading", function() {
-    const elements = document.querySelectorAll("*");
-
-    elements.forEach(element => {
-        element.style.opacity = 1;
-    });
-
-    document.body.style.display = "flex"; 
+    loaderFunction();
 });
 
 let isFilterApplied = false;
